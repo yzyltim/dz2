5a4efc5a79cf19566597aaf623cf7664dfaac9b8
38067 <tima.sk77@gmail.com>
2025-05-10 12:02:46 +0300

    jj

---
+++ zadacha1.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self, account_number, balance=0.0):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Недостатньо коштів на рахунку.")
