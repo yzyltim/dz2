d2a468f4c67b7fa8ca8ee3e32d4899765267efd5
38067 <tima.sk77@gmail.com>
2025-05-10 12:08:57 +0300

    jj

---
+++ zadacha3.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, name, position, salary):
+        self.name = name
+        self.position = position
+        self.salary = salary
+
+    def get_salary_info(self):
+        return f"Заробітна плата {self.name}: {self.salary}"
